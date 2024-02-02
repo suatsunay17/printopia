a6c8f6e80bccbbab90e99440225b5e42986804c0
Suat Sunay <95652997+suatsunay17@users.noreply.github.com>
2024-02-02 22:07:11 +0200

    create home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import Header from './pages/Header/Header'
+import Home from './pages/Home/Home'
 
 function App() {
 
   return (
     <>
      <Header/>
+     <Home/>
     </>
   )
 }
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import "./Home.scss";
+
+import printer1 from "../../assets/printer1.jpg";
+import printer2 from "../../assets/printer2.jpg";
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="welcome">
+        <h1>
+          Welcome to <span>Printopia.</span>
+        </h1>
+        <h2>Where Imagination Takes Physical Form</h2>
+        <h3>Explore the Gallery</h3>
+        <p>
+          Dive into a diverse world of 3D prints, where creativity meets
+          precision. From intricate figurines to revolutionary prototypes, each
+          piece is a testament to the limitless possibilities of 3D printing
+          technology. Browse our gallery to get inspired, or showcase your own
+          masterpieces to the world.
+        </p>
+        <h3>Join the Community</h3>
+        <p>
+          Connect with fellow printing enthusiasts in the Printopia Forum.
+          Exchange ideas, seek advice, troubleshoot, or just chat about the
+          latest trends in 3D printing. Our community is a melting pot of
+          experts, hobbyists, and newcomers alike, all united by a passion for
+          creation.
+        </p>
+      </div>
+      <div className="images">
+        <img src={printer1} alt="printer" className="img1" />
+        <img src={printer2} alt="printer" className="img2" />
+      </div>
+    </div>
+  );
+}
+
+export default Home;
