ee897f40211d63b931858768abbd7baebb568e58
Suat Sunay <95652997+suatsunay17@users.noreply.github.com>
2024-02-03 15:53:55 +0200

    add footer

---
+++ src/pages/Footer/Footer.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./Footer.scss"; // Assuming you have a separate SCSS file for styling your footer
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <section className="footer-section">
+        <ul>
+          <li>
+          <i class="fa-brands fa-instagram"></i>
+          </li>
+          <li>
+          <i class="fa-brands fa-linkedin"></i>
+          </li>
+          <li>
+          <i class="fa-brands fa-github"></i>
+          </li>
+        </ul>
+      </section>
+
+      <div className="footer-bottom">
+        <p>&copy; {new Date().getFullYear()} Printopia. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
