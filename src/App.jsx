5987af151a15c9052b8fc568c409cc04ec11c609
Suat Sunay <95652997+suatsunay17@users.noreply.github.com>
2024-02-03 15:53:24 +0200

    update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,36 @@
-import { useState } from 'react'
-import Header from './pages/Header/Header'
-import Home from './pages/Home/Home'
+import './App.css'
+import Header from "./pages/Header/Header";
+import Home from "./pages/Home/Home";
+import Footer from "./pages/Footer/Footer";
+import Gallery from "./components/Gallery/Gallery";
+import Forum from "./components/Forum/Forum";
+import NoMatch from "./pages/NoMatch/NoMatch";
+
+import { Routes, Route, Outlet } from "react-router-dom";
 
 function App() {
-
   return (
-    <>
-     <Header/>
-     <Home/>
-    </>
-  )
+    <div className="app">
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="gallery" element={<Gallery />} />
+          <Route path="forum" element={<Forum />} />
+          <Route path="*" element={<NoMatch />} />
+        </Route>
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+function Layout() {
+  return (
+    <>
+      <Header />
+      <Outlet/>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
