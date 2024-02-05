4afc6883bccef61783368cd899b9f75883d77f16
Suat Sunay <95652997+suatsunay17@users.noreply.github.com>
2024-02-05 14:16:06 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
+import firebase from "firebase/compat/app";
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -11,9 +11,13 @@ const firebaseConfig = {
   projectId: "printopia-48a56",
   storageBucket: "printopia-48a56.appspot.com",
   messagingSenderId: "587237707405",
-  appId: "1:587237707405:web:1725efa81a792e981d9cc4"
+  appId: "1:587237707405:web:1725efa81a792e981d9cc4",
 };
 
+let app;
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app)
+if (firebase.apps.length === 0) {
+  app = initializeApp(firebaseConfig);
+} else {
+  app = firebase.app();
+}
