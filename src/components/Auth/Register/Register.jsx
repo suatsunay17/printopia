0a4829f46a16ef36ccfe316eedb6ec3710b3cf38
Suat Sunay <95652997+suatsunay17@users.noreply.github.com>
2024-02-05 14:16:02 +0200

    create register component

---
+++ src/components/Auth/Register/Register.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+import "./Register.scss";
+
+function Register() {
+  const [userData, setUserData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setUserData({ ...userData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const auth = getAuth();
+    createUserWithEmailAndPassword(auth, userData.email, userData.password)
+      .then((userCredential) => {
+        // Signed up
+        const user = userCredential.user;
+        // Additional actions upon successful registration
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.error("Registration error:", errorCode, errorMessage);
+        // Display or log the error message
+      });
+  };
+
+  return (
+    <div className="reg-main">
+      <div className="register-form">
+        <h2>Register</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={userData.email}
+              onChange={handleChange}
+              required
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={userData.password}
+              onChange={handleChange}
+              required
+            />
+          </div>
+
+          <button type="submit">Register</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Register;
