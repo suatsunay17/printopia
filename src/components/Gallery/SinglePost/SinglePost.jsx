a7b2479a98eff2024f5fe85cae8bcf859cb05963
Suat Sunay <95652997+suatsunay17@users.noreply.github.com>
2024-02-05 13:32:30 +0200

    gallery css

--- src/components/Gallery/SinglePost/SinglePost.jsx
+++ src/components/Gallery/SinglePost/SinglePost.jsx
@@ -1,12 +1,25 @@
 import React from "react";
-import './SinglePost.scss';
+import "./SinglePost.scss";
 
-function SinglePost( {item} ) {
+function SinglePost({ item }) {
   return (
-    <div className="single-post">
-      <h4>{item.name}</h4>
-      <img src={item.img} alt="" />
+    <>
+    <div className="container">
+    <div className="row">
+      <div className="col-md-6 col-sm-8 col-xs-12 col-md-offset-3 col-sm-offset-2">
+        <div className="card" >
+          <div className="image">
+            <img src={item.img} width={'100%'}/>
+          </div>
+          <div className="text">
+            <h3>{item.name}</h3>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
+    </>
+   
   );
 }
 
