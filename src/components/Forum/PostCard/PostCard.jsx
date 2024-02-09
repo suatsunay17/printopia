efdbfb1f76a4353199720369321a5029faa58d0a
Suat Sunay <95652997+suatsunay17@users.noreply.github.com>
2024-02-09 13:23:58 +0200

    create postCard

---
+++ src/components/Forum/PostCard/PostCard.jsx
@@ -0,0 +1,14 @@
+// PostCard.jsx
+import React from 'react';
+import './PostCard.scss'
+
+const PostCard = ({ post }) => {
+  return (
+    <div className="post-card">
+      <h3>{post.title}</h3>
+      <p>{post.content}</p>
+    </div>
+  );
+};
+
+export default PostCard;
