0c7392702920e57fae58330b103eeb8790cfa85f
Suat Sunay <95652997+suatsunay17@users.noreply.github.com>
2024-02-09 13:24:10 +0200

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,12 +4,14 @@ import {
   onAuthStateChanged,
   signOut,
   signInWithEmailAndPassword,
-  createUserWithEmailAndPassword
+  createUserWithEmailAndPassword,
 } from "firebase/auth";
 import { getFirebaseConfig } from "./firebase-config";
+import { getFirestore } from "firebase/firestore";
 
 const app = initializeApp(getFirebaseConfig());
 const auth = getAuth(app);
+const db = getFirestore(app);
 
 export const signInUser = async (email, password) => {
   if (!email || !password) return;
@@ -28,3 +30,4 @@ export const userStateListener = (callback) => {
 };
 
 export const SignOutUser = async () => await signOut(auth);
+export { db };
