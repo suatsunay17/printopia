5fe3a3d7b4bd1f8c48b77906b45094cb94ff2e04
Suat Sunay <95652997+suatsunay17@users.noreply.github.com>
2024-02-09 12:32:08 +0200

    Update firebase-config.js

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -1,10 +1,10 @@
 const config = {
-  apiKey: "AIzaSyC_ZhGLSMo8vX_18bSDnAwe7mw7qtxL4Qo",
-  authDomain: "printopia-48a56.firebaseapp.com",
-  projectId: "printopia-48a56",
-  storageBucket: "printopia-48a56.appspot.com",
-  messagingSenderId: "587237707405",
-  appId: "1:587237707405:web:1725efa81a792e981d9cc4",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 export function getFirebaseConfig() {
