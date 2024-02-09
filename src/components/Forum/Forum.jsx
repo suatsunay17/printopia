3946ab781eeb155fe5ef1bb1e2b875dddf34eeea
Suat Sunay <95652997+suatsunay17@users.noreply.github.com>
2024-02-09 13:23:53 +0200

    update forum

--- src/components/Forum/Forum.jsx
+++ src/components/Forum/Forum.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import PostsList from "./PostsList/PostsList";
+import "./Forum.scss";
 
 function Forum() {
   return (
-    <div>Forum</div>
-  )
+    <div className="forum">
+      <button>
+        <Link to={"/create"}>Create Post</Link>
+      </button>
+      <PostsList />
+    </div>
+  );
 }
 
-export default Forum
+export default Forum;
