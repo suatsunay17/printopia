258c94a9f732b3368ec30cdfb925094f6dae6380
Suat Sunay <95652997+suatsunay17@users.noreply.github.com>
2024-02-09 13:56:14 +0200

    render timestamp

--- src/components/Forum/PostCard/PostCard.jsx
+++ src/components/Forum/PostCard/PostCard.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
-import './PostCard.scss'
+import React from "react";
+import "./PostCard.scss";
 
 const PostCard = ({ post }) => {
+  const formattedDate = post.createdAt
+    ? new Date(post.createdAt.toDate()).toLocaleString()
+    : "";
   return (
     <div className="post-card">
       <h3>{post.title}</h3>
       <p>{post.content}</p>
+      <p className="post-author">
+        Posted by: {post?.createdBy?.displayName || "Anonymous user"} on {formattedDate}
+      </p>
     </div>
   );
 };
