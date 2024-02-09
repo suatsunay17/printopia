a71079e5ddd76ee36991305d23d31175efce657d
Suat Sunay <95652997+suatsunay17@users.noreply.github.com>
2024-02-09 13:24:07 +0200

    create post list

---
+++ src/components/Forum/PostsList/PostsList.jsx
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from "react";
+import { db } from "../../../firebase/firebase"; // Adjust this path to your firebase.js file
+import { collection, getDocs, query, orderBy } from "firebase/firestore";
+import PostCard from "../PostCard/PostCard";
+import './PostsList.scss'
+
+const PostsList = () => {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+      try {
+        const querySnapshot = await getDocs(
+          query(collection(db, "posts"), orderBy("createdAt", "desc"))
+        );
+        const postList = querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setPosts(postList);
+      } catch (error) {
+        console.error("Error fetching posts:", error);
+      }
+      setLoading(false);
+    };
+
+    fetchPosts();
+  }, []);
+
+  if (loading) {
+    return <p>Loading posts...</p>;
+  }
+
+  return (
+    <div className="forum-posts">
+      <h2>Forum Posts</h2>
+      {posts.length > 0 ? (
+        <ul>
+          {posts.map((post) => (
+            <li key={post.id}>
+              <PostCard post={post}/>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>No posts available.</p>
+      )}
+    </div>
+  );
+};
+
+export default PostsList;
