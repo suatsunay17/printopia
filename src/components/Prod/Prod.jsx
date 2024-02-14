74b1ba431db526289bd4877eb6d6ffdc5aacbf0a
Suat Sunay <95652997+suatsunay17@users.noreply.github.com>
2024-02-14 14:46:32 +0200

    Create Prod.jsx

---
+++ src/components/Prod/Prod.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Prod() {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        padding: "20px",
+        backgroundColor: "#f8d7da",
+        color: "#721c24",
+        border: "1px solid #f5c6cb",
+        borderRadius: "4px",
+      }}
+    >
+      <h3>Under Development</h3>
+      <p>This project is currently under development.</p>
+      <p>Expect bugs and unfinished functionality.</p>
+    </div>
+  );
+}
+
+export default Prod;
