89e790eabe3215a70f8cfed38cb7c636d260c3d7
Suat Sunay <95652997+suatsunay17@users.noreply.github.com>
2024-02-19 18:03:42 +0200

    primereact provider

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,16 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext.jsx";
+import { PrimeReactProvider } from 'primereact/api';
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
+        <PrimeReactProvider>
         <App />
+        </PrimeReactProvider>
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
