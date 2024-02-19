f380508c00fb51566a25b0bbf1268f56d1eb8b14
Suat Sunay <95652997+suatsunay17@users.noreply.github.com>
2024-02-19 18:04:01 +0200

    Create HomeCarousel.jsx

---
+++ src/pages/Home/HomeCarousel.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Carousel } from "primereact/carousel";
+
+import img1 from "../../assets/img1.jpg";
+import img2 from "../../assets/img2.jpg";
+import img3 from "../../assets/img3.jpg";
+import img4 from "../../assets/img4.jpg";
+import img5 from "../../assets/img5.jpg";
+
+let imgs = [img1, img2, img3, img4, img5];
+
+function HomeCarousel() {
+  const productTemplate = (imgs) => {
+    return (
+      <div className=" p-d-flex p-ai-center p-jc-center">
+        <img
+          src={imgs}
+          style={{ width: "400px", height: "300px", objectFit: "cover" }}
+        />
+      </div>
+    );
+  };
+
+  return (
+    <div className=" flex justify-content-center">
+      <Carousel
+        value={imgs}
+        numVisible={1}
+        numScroll={1}
+        orientation="vertical"
+        verticalViewPortHeight="300px"
+        itemTemplate={productTemplate}
+      />
+    </div>
+  );
+}
+
+export default HomeCarousel;
