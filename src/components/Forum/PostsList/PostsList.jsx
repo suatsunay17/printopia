968bf4f62dc476969b1ee895a4212a4d28994623
Suat Sunay <95652997+suatsunay17@users.noreply.github.com>
2024-02-21 17:23:22 +0200

    Update PostsList.jsx

--- src/components/Forum/PostsList/PostsList.jsx
+++ src/components/Forum/PostsList/PostsList.jsx
@@ -1,39 +1,7 @@
-import React, { useState, useEffect } from "react";
-import db from "../../../firebase/firebase";
-import { collection, getDocs, query, orderBy } from "firebase/firestore";
 import PostCard from "../PostCard/PostCard";
 import "./PostsList.scss";
-import { Link } from "react-router-dom";
 
-const PostsList = () => {
-  const [posts, setPosts] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const fetchPosts = async () => {
-      setLoading(true);
-      try {
-        const querySnapshot = await getDocs(
-          query(collection(db, "posts"), orderBy("createdAt", "desc"))
-        );
-        const postList = querySnapshot.docs.map((doc) => ({
-          id: doc.id,
-          ...doc.data(),
-        }));
-        setPosts(postList);
-      } catch (error) {
-        console.error("Error fetching posts:", error);
-      }
-      setLoading(false);
-    };
-
-    fetchPosts();
-  }, []);
-
-  if (loading) {
-    return <p>Loading posts...</p>;
-  }
-
+const PostsList = ({posts}) => {
   return (
     <div className="forum-posts">
       <h2>Forum Posts</h2>
@@ -41,7 +9,7 @@ const PostsList = () => {
         <ul>
           {posts.map((post) => (
             <li key={post.id}>
-                <PostCard post={post} />
+              <PostCard post={post} />
             </li>
           ))}
         </ul>
