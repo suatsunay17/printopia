8ead1112caa69458f52690ae863745cce4504a43
Suat Sunay <95652997+suatsunay17@users.noreply.github.com>
2024-02-22 10:48:25 +0200

    lift state

--- src/components/Forum/Forum.jsx
+++ src/components/Forum/Forum.jsx
@@ -5,31 +5,7 @@ import "./Forum.scss";
 import db from "../../firebase/firebase";
 import { collection, getDocs, query, orderBy } from "firebase/firestore";
 
-function Forum() {
-  const [posts, setPosts] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const fetchPosts = async () => {
-      setLoading(true);
-      try {
-        const querySnapshot = await getDocs(
-          query(collection(db, "posts"), orderBy("createdAt", "desc"))
-        );
-        const postList = querySnapshot.docs.map((doc) => ({
-          id: doc.id,
-          ...doc.data(),
-        }));
-        setPosts(postList);
-      } catch (error) {
-        console.error("Error fetching posts:", error);
-      }
-      setLoading(false);
-    };
-
-    fetchPosts();
-  }, []);
-
+function Forum({ posts, loading }) {
   if (loading) {
     return <p>Loading posts...</p>;
   }
