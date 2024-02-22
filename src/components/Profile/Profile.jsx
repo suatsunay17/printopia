76183b614e3da3554bcbe8100d1c85c4370bf1ad
Suat Sunay <95652997+suatsunay17@users.noreply.github.com>
2024-02-22 10:48:15 +0200

    users see posts they've created

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,18 +1,40 @@
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
+import db from "../../firebase/firebase";
 import { AuthContext } from "../../context/AuthContext";
+import PostCard from "../Forum/PostCard/PostCard";
+import { collection, query, where, getDocs } from "firebase/firestore";
 
-function Profile() {
+function Profile({ posts }) {
   const { currentUser, signOut } = useContext(AuthContext);
+  const [userPosts, setUserPosts] = useState([]);
+  const currentUserId = currentUser.uid;
+  let currentPosts = [];
+
+  useEffect(() => {
+    for (const el of posts) {
+      if (el.createdBy.uid === currentUserId) {
+        currentPosts.push(el);
+      }
+    }
+    setUserPosts(currentPosts);
+  }, []);
+
+  console.log(userPosts);
 
   return (
-    /**
-     * Extract the currrentUser from the context, if you want to
-     * get the User info, like the email, display name, etc.
-     */
     <div>
       <h3>Welcome! {currentUser?.email}</h3>
       <p>Sign In Status: {currentUser && "active"}</p>
       <button onClick={signOut}>Sign Out</button>
+
+      <h2>User's Posts</h2>
+      <div className="post-list">
+        {userPosts.length > 0 ? (
+          userPosts.map((post) => <PostCard key={post.id} post={post} />)
+        ) : (
+          <p>No posts found.</p>
+        )}
+      </div>
     </div>
   );
 }
