cbace034d48e6d008f8f2d7f12774d4d4020c43c
Suat Sunay <95652997+suatsunay17@users.noreply.github.com>
2024-03-02 11:08:27 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,26 @@
-import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-  return {
-    define: {
-      'process.env.REACT_APP_API_KEY': JSON.stringify(env.REACT_APP_API_KEY),
-      'process.env.REACT_APP_AUTH_DOMAIN': JSON.stringify(env.REACT_APP_AUTH_DOMAIN),
-      'process.env.REACT_APP_PROJECT_ID': JSON.stringify(env.REACT_APP_PROJECT_ID),
-      'process.env.REACT_APP_STORAGE_BUCKET': JSON.stringify(env.REACT_APP_STORAGE_BUCKET),
-      'process.env.REACT_APP_APP_ID': JSON.stringify(env.REACT_APP_APP_ID),
-    },
-    plugins: [react()],
-  }
-})
+  const env = loadEnv(mode, process.cwd(), "");
+  return (
+    {
+      define: {
+        "process.env.REACT_APP_API_KEY": JSON.stringify(env.REACT_APP_API_KEY),
+        "process.env.REACT_APP_AUTH_DOMAIN": JSON.stringify(
+          env.REACT_APP_AUTH_DOMAIN
+        ),
+        "process.env.REACT_APP_PROJECT_ID": JSON.stringify(
+          env.REACT_APP_PROJECT_ID
+        ),
+        "process.env.REACT_APP_STORAGE_BUCKET": JSON.stringify(
+          env.REACT_APP_STORAGE_BUCKET
+        ),
+        "process.env.REACT_APP_APP_ID": JSON.stringify(env.REACT_APP_APP_ID),
+      },
+      plugins: [react()],
+    }
+    
+  );
+});
